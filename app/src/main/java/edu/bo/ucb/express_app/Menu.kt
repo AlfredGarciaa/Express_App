281b7a26c1a8ca31f64085fa986980eb0f138bc3
Sergio <sergio.salinas.b@ucb.edu.bo>
2022-11-17 15:30:45 -0400

    menu

---
+++ app/src/main/java/edu/bo/ucb/express_app/Menu.kt
@@ -0,0 +1,47 @@
+package edu.bo.ucb.express_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatDrawableManager.get
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class Menu : AppCompatActivity() {
+    lateinit var rvButtons : RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        rvButtons = findViewById(R.id.rv_buttons)
+        val list = arrayListOf<Int>(
+            R.drawable.configuraciones,
+            R.drawable.alarmas,
+            R.drawable.camara,
+            R.drawable.contactos,
+            R.drawable.galeria,
+            R.drawable.horario,
+        )
+        val list2= arrayListOf<String>(
+            getString(R.string.Configuraciones),
+            getString(R.string.Alarmas),
+            getString(R.string.Cámara),
+            getString(R.string.Contactos),
+            getString(R.string.Galeria),
+            getString(R.string.Horario)
+        )
+        val list3= arrayListOf<Intent>(
+            Intent(this, MainActivity::class.java) ,
+            Intent(this, MainActivity::class.java),
+            Intent(this, MainActivity::class.java),
+            Intent(this, MainActivity::class.java),
+            Intent(this, MainActivity::class.java),
+            Intent(this, MainActivity::class.java),
+        )
+        val gridLayout = GridLayoutManager(this,2)
+        gridLayout.orientation = GridLayoutManager.VERTICAL
+        rvButtons.layoutManager = gridLayout
+        rvButtons.adapter = MenuAdapter(list,list2,list3, applicationContext)
+
+    }
+}
---
+++ app/src/main/java/edu/bo/ucb/express_app/MenuAdapter.kt
@@ -0,0 +1,39 @@
+package edu.bo.ucb.express_app
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MenuAdapter(val list: ArrayList<Int>,val list2: ArrayList<String>,val list3: ArrayList<Intent>,val applicationContext: Context?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.menu_row, parent, false)
+        return UserListViewHolder(view)
+
+    }
+
+    class UserListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = list.get(position)
+        val item2= list2.get(position)
+        val item3= list3.get(position)
+        holder.itemView.findViewById<ImageView>(R.id.rv_option1).setImageResource(item)
+        holder.itemView.findViewById<TextView>(R.id.textView1).text= item2
+        holder.itemView.setOnClickListener{
+            item3.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            applicationContext?.startActivity(item3) }
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+}

799e88195d2478c15b312fdf8ff1cd8e934a8646
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-29 16:25:28 -0400

    Refactoring de Camara - sigue fallando el solo abrir

---
+++ app/src/main/java/edu/bo/ucb/express_app/Camara/CamaraAdapter.kt
@@ -0,0 +1,51 @@
+package edu.bo.ucb.express_app.Camara
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import edu.bo.ucb.express_app.R
+import edu.bo.ucb.express_app.databinding.ActivityCamaraBinding
+
+class CamaraAdapter : AppCompatActivity() {
+    private lateinit var binding: ActivityCamaraBinding
+    val REQUEST_CODE_TAKE_PHOTO = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camara)
+
+        binding = ActivityCamaraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupListener()
+    }
+
+    private fun setupListener() {
+        binding.btnTakePhoto.setOnClickListener {
+            openActivityForResult()
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePicture ->
+            takePicture.resolveActivity(packageManager)?.also {
+                openActivityForResult()
+            }
+        }
+    }
+
+    fun openActivityForResult() {
+        startForResult.launch(Intent(this, CamaraAdapter::class.java))
+    }
+
+    val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+            // Handle the Intent
+            //do stuff here
+        }
+    }
+}

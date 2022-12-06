4f8518403bf02105e4096861c4e1f70c95bfef6d
Alfred <alfred.garcia@ucb.edu.bo>
2022-12-06 19:21:29 -0400

    Video PLayer Completo

--- app/src/main/java/edu/bo/ucb/express_app/Login/LoginAdapter.kt
+++ app/src/main/java/edu/bo/ucb/express_app/Login/LoginAdapter.kt
@@ -2,9 +2,12 @@ package edu.bo.ucb.express_app.Login
 
 import android.app.AlertDialog
 import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import edu.bo.ucb.express_app.Menu.Menu
@@ -13,9 +16,32 @@ import edu.bo.ucb.express_app.Welcome.WelcomeAdapter
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginAdapter : AppCompatActivity() {
+
+    protected lateinit var vv_fondo: VideoView
+    protected lateinit var mMediaPlayer: MediaPlayer
+    protected var mCurrentVideoPosition: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        vv_fondo = findViewById<VideoView>(R.id.vv_fondo)
+        val uri = Uri.parse("android.resource://"
+                +packageName
+                +"/"
+                +R.raw.cascada)
+
+        vv_fondo.setVideoURI(uri)
+        vv_fondo.start()
+        vv_fondo.setOnPreparedListener { mp ->
+            mMediaPlayer = mp
+            mMediaPlayer.isLooping = true
+            if (mCurrentVideoPosition != 0) {
+                mMediaPlayer.seekTo(mCurrentVideoPosition)
+
+                mMediaPlayer.start()
+            }
+        }
 
         // Setup \\
         setup()
@@ -79,5 +105,22 @@ class LoginAdapter : AppCompatActivity() {
             putExtra("email", email)
         }
         startActivity(menuIntent)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mCurrentVideoPosition = mMediaPlayer.currentPosition
+        vv_fondo.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        vv_fondo.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMediaPlayer.release()
+        //mMediaPlayer = null
     }
 }

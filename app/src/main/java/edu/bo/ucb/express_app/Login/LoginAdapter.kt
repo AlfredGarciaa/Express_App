c266a2c1b831f182025710a7e8001021daca69ba
Alfred <alfred.garcia@ucb.edu.bo>
2022-12-06 18:12:36 -0400

    Refactoring de App

--- app/src/main/java/edu/bo/ucb/express_app/Login/LoginAdapter.kt
+++ app/src/main/java/edu/bo/ucb/express_app/Login/LoginAdapter.kt
@@ -1,24 +1,24 @@
 package edu.bo.ucb.express_app.Login
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import edu.bo.ucb.express_app.Menu.Menu
 import edu.bo.ucb.express_app.R
 import edu.bo.ucb.express_app.Welcome.WelcomeAdapter
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginAdapter : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val tvGoToRegister = findViewById<TextView>(R.id.tv_go_to_register)
-        tvGoToRegister.setOnClickListener{
-            goToRegister()
-        }
+        // Setup \\
+        setup()
 
         // CAMBIAR VISTA DE LOGIN A WELCOME \\
         val welcome = findViewById<View>(R.id.btn_welcome) as Button
@@ -26,15 +26,58 @@ class LoginAdapter : AppCompatActivity() {
             startActivity(Intent(this, WelcomeAdapter::class.java))
         }
 
+        /*
         // CAMBIAR VISTA DE LOGIN A MENU -> AL LOGEARSE \\
-        val menu_ = findViewById<View>(R.id.btn_menu_) as Button
-        menu_.setOnClickListener {
+        val menu = findViewById<View>(R.id.btn_menu) as Button
+        menu.setOnClickListener {
             startActivity(Intent(this, Menu::class.java))
+        }
+        */
+    }
+
+    private fun setup() {
+        btn_register.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showMenu(it.result?.user?.email ?: "")
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+        btn_acceder.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val menu = findViewById<View>(R.id.btn_menu) as Button
+                        menu.setOnClickListener {
+                            startActivity(Intent(this, Menu::class.java))
+                        }
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
         }
     }
 
-    private fun goToRegister(){
-        val i = Intent(this, RegisterActivity::class.java)
-        startActivity(i)
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showMenu(email: String) {
+        val menuIntent = Intent(this, Menu::class.java).apply {
+            putExtra("email", email)
+        }
+        startActivity(menuIntent)
     }
 }
--- app/src/main/java/edu/bo/ucb/express_app/Welcome/WelcomeAdapter.kt
+++ app/src/main/java/edu/bo/ucb/express_app/Welcome/WelcomeAdapter.kt
@@ -3,17 +3,13 @@ package edu.bo.ucb.express_app.Welcome
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import edu.bo.ucb.express_app.Login.LoginAdapter
-import edu.bo.ucb.express_app.Login.RegisterActivity
 import edu.bo.ucb.express_app.R
 
 class WelcomeAdapter : AppCompatActivity() {
     val loginButton:Button
         get() = findViewById(R.id.Login)
-    val signUp:TextView
-        get() = findViewById(R.id.signUp)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -21,9 +17,6 @@ class WelcomeAdapter : AppCompatActivity() {
 
         loginButton.setOnClickListener{
             startActivity(Intent(this,LoginAdapter::class.java))
-        }
-        signUp.setOnClickListener{
-            startActivity(Intent(this,RegisterActivity::class.java))
         }
     }
 }

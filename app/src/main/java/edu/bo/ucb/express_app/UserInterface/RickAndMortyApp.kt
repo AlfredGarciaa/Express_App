39dfa5a4cbe7fa62ccbd5e10cd687a91bd54880f
Alfred <alfred.garcia@ucb.edu.bo>
2022-12-08 10:57:20 -0400

    Dependencias externas compiladas

---
+++ app/src/main/java/edu/bo/ucb/express_app/UserInterface/RickAndMortyApp.kt
@@ -0,0 +1,22 @@
+package edu.bo.ucb.express_app.UserInterface
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.compose.rememberNavController
+import edu.bo.ucb.express_app.UserInterface.Theme.RickAndMortyTheme
+
+@Composable
+fun RickAndMortyApp() {
+    RickAndMortyTheme {
+        val navController = rememberNavController()
+        val navigationActions = remember(navController) {
+            RickAndMortyActions(navController)
+        }
+
+        RickAndMortyNavGraph(
+            navController = navController,
+            navigateToHome = navigationActions.navigateToHome,
+            navigateToDetail = navigationActions.navigateToDetail
+        )
+    }
+}
---
+++ app/src/main/java/edu/bo/ucb/express_app/UserInterface/Theme/Type.kt
@@ -0,0 +1,4 @@
+package edu.bo.ucb.express_app.UserInterface.Theme
+
+class Type {
+}

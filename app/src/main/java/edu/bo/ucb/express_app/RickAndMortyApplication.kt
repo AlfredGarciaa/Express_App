90d198a5965d36ebbfd55972ecc03daedc32229e
Alfred <alfred.garcia@ucb.edu.bo>
2022-12-08 15:51:25 -0400

    REESTRUCTURACION DE PROYECTO

--- app/src/main/java/edu/bo/ucb/express_app/RickAndMortyApplication.kt
+++ app/src/main/java/edu/bo/ucb/express_app/RickAndMortyApplication.kt
@@ -1,8 +1,9 @@
 package edu.bo.ucb.express_app
-
+/*
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 
 class RickAndMortyApplication: Application()
+*/

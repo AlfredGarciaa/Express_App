81adf5c2755d9d56ae046d7891c166d0eee4bcf4
Alfred <alfred.garcia@ucb.edu.bo>
2022-12-08 11:43:40 -0400

    Funcionalidades agregadas a funcionalidades multiples

---
+++ app/src/main/java/edu/bo/ucb/express_app/UserInterface/Detail/DetailState.kt
@@ -0,0 +1,8 @@
+package edu.bo.ucb.express_app.UserInterface.Detail
+
+import edu.bo.ucb.express_app.Domain.Model.Character
+
+data class DetailState(
+    val character: Character? = null,
+    val isLoading: Boolean = false
+)
---
+++ app/src/main/java/edu/bo/ucb/express_app/UserInterface/Home/HomeState.kt
@@ -0,0 +1,10 @@
+package edu.bo.ucb.express_app.UserInterface.Home
+
+import edu.bo.ucb.express_app.Domain.Model.Characters
+
+data class HomeState(
+    val characters: List<Characters> = emptyList(),
+    val showPrevious: Boolean = false,
+    val showNext: Boolean = false,
+    val isLoading: Boolean = false
+)
